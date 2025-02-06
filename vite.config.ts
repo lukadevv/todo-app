d341e044a3331c2473180106e0b24b272a5957a5
Luka <lukawolf.dev@gmail.com>
2025-02-06 09:00:13 -0300

    PWA and some changes
    + PWA
    + Title and favicon were added.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,37 @@
 import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [
+    preact(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "Task App",
+        short_name: "todo-app",
+        icons: [
+          {
+            src: "/assets/metadata/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/assets/metadata/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+        theme_color: "#db6b42",
+        background_color: "#db6b42",
+        display: "standalone",
+      },
+    }),
+  ],
   server: {
     port: 4209,
   },
